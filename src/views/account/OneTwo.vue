b259e764f681443490165466f02a53a6544a436c
2500907739@qq.com <2500907739@qq.com>
2023-02-10 16:37:17 +0800

    分类表单

--- src/views/account/OneTwo.vue
+++ src/views/account/OneTwo.vue
@@ -3,7 +3,7 @@
     <el-button type="danger">添加一级分类</el-button>
     <hr class="spacing-hr" />
     <el-row>
-      <el-col :span="6">
+      <el-col :span="10">
         <div class="category-list">
           <el-tree
             @node-click="handlerNodeClick"
@@ -25,7 +25,20 @@
           </el-tree>
         </div>
       </el-col>
-      <el-col :span="18">输入框</el-col>
+      <el-col :span="10">
+        <h4 class="column">分类名称</h4>
+        <el-form label-width="100px">
+          <el-form-item label="父级分类">
+            <el-input style="width: 20%"></el-input>
+          </el-form-item>
+          <el-form-item label="子级分类">
+            <el-input style="width: 20%"></el-input>
+          </el-form-item>
+          <el-form-item label="">
+            <el-button type="danger">确定</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -89,5 +102,13 @@ button {
   margin: 8px 3px;
   font-size: 12px;
   height: auto;
+}
+.column {
+  height: 44px;
+  padding: 0 20px;
+  margin-bottom: 30px;
+  line-height: 44px;
+  border-radius: 6px;
+  background-color: #f3f3f3;
 }
 </style>
